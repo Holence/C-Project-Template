2e63fde008ac8436e01406516a8ef7ad053cdd9e
Holence <Holence08@gmail.com>
2025-09-19 14:44:39 +0800

    flex/bison Makefile addon

---
+++ scripts/addon/flex-bison.mk
@@ -0,0 +1,38 @@
+# Copyright (C) 2025 Holence <Holence08@gmail.com>
+
+# this file should be included before
+# - `include scripts/build.mk`
+# - `include scripts/kconfig-build.mk`
+
+# Your lexer.l
+FLEX_SRC  ?=
+# Your parser.y
+BISON_SRC ?=
+
+FLEX  ?= flex
+BISON ?= bison
+
+# Generated Files
+FLEX_C  := $(FLEX_SRC:%.l=%.lex.c)
+BISON_C := $(BISON_SRC:%.y=%.tab.c)
+BISON_H := $(BISON_SRC:%.y=%.tab.h)
+
+SRCS += $(FLEX_C) $(BISON_C)
+
+LDFLAGS += -lfl
+
+# Bison generates parser .tab.c and .tab.h
+%.tab.c: %.y
+	@echo "+ YACC $< -> $@"
+	$(Q)$(BISON) --no-lines --debug -o $(BISON_C) $<
+%.tab.h: %.y
+	@echo "+ YACC $< -> $@"
+	$(Q)$(BISON) --header=$(BISON_H) $< -o /dev/null
+
+# Flex generates .lex.c
+%.lex.c: %.l $(BISON_H)
+	@echo "+ LEX $< -> $@"
+	$(Q)$(FLEX) --noline -o $@ $<
+
+clean::
+	-$(RM) $(FLEX_C) $(BISON_C) $(BISON_H)
