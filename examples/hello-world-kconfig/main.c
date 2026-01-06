074ad8e5003e4c21bb5ec452ed7b00a2cbf7004f
Holence <Holence08@gmail.com>
2026-01-06 13:55:17 +0800

    more example for using utils

--- examples/hello-world-kconfig/main.c
+++ examples/hello-world-kconfig/main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include "generated/autoconf.h"
 
+#include "utils/debug.h"
+#include "utils/hex.h"
+#include <string.h>
+#include <stdlib.h>
+
 int main(int argc, char const *argv[]) {
-    printf("Hello world, I'm %s\n", CONFIG_MY_NAME);
+    Log("Hello world, I'm %s", CONFIG_MY_NAME);
+
     for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, argv[i]);
     }
+
+    char s1[] = "this string lives on stack";
+    hex_dump(s1, strlen(s1), 8);
+    ColorPrintByteArray(ANSI_FG_GREEN, s1, strlen(s1));
+
+    char *s2 = malloc(32);
+    strcpy(s2, "this string lives on heap");
+    hex_dump(s2, strlen(s2), 8);
+    ColorPrintByteArray(ANSI_FG_MAGENTA, s2, strlen(s2));
+    free(s2);
+
+    char *s3 = "this string lives on .rodata";
+    hex_dump(s3, strlen(s3), 8);
+    ColorPrintByteArray(ANSI_FG_BLACK, s3, strlen(s3));
+
     return 0;
 }
--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 
+#define CONFIG_ENABLE_DEBUG_LOGGING 1
+#include "utils/debug.h"
+#include "utils/hex.h"
+#include <string.h>
+#include <stdlib.h>
+
 int main(int argc, char const *argv[]) {
-    printf("Hello world\n");
+    Log("Hello world");
+
     for (int i = 0; i < argc; i++) {
         printf("argv[%d]: %s\n", i, argv[i]);
     }
+
+    char s1[] = "this string lives on stack";
+    hex_dump(s1, strlen(s1), 8);
+    ColorPrintByteArray(ANSI_FG_GREEN, s1, strlen(s1));
+
+    char *s2 = malloc(32);
+    strcpy(s2, "this string lives on heap");
+    hex_dump(s2, strlen(s2), 8);
+    ColorPrintByteArray(ANSI_FG_MAGENTA, s2, strlen(s2));
+    free(s2);
+
+    char *s3 = "this string lives on .rodata";
+    hex_dump(s3, strlen(s3), 8);
+    ColorPrintByteArray(ANSI_FG_BLACK, s3, strlen(s3));
+
     return 0;
 }
