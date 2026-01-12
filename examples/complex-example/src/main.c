8006701a12ea10c7a200ca7f1361080a64cc6e4d
Holence <Holence08@gmail.com>
2026-01-12 23:00:52 +0800

    add more example

---
+++ examples/complex-example/src/main.c
@@ -0,0 +1,8 @@
+#include "liba/liba.h"
+#include "libb/libb.h"
+
+int main(int argc, char const *argv[]) {
+    func_a();
+    func_b();
+    return 0;
+}
---
+++ examples/complex-example/third-party/liba/liba.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void func_a() {
+    printf("hello from liba!\n");
+}
