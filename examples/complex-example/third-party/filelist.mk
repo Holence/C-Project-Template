a479ceb92780cc1107d809360df8c6858ea5d182
Holence <Holence08@gmail.com>
2026-01-14 17:55:20 +0800

    demonstrate `BUILD_DIR = .` in examples

--- examples/complex-example/third-party/filelist.mk
+++ examples/complex-example/third-party/filelist.mk
@@ -1,16 +1,16 @@
 # 这里需要依赖 FORCE 是因为 target 是文件
 # 如果没有 FORCE, 那么如果文件存在, 就不会进去 make 了
 
-LIBS-$(CONFIG_USE_LIBA) += third-party/liba/build/liba.a
+LIBS-$(CONFIG_USE_LIBA) += third-party/liba/liba.a
 # liba.a 的制作需要进入 liba 目录去 make
-third-party/liba/build/liba.a: FORCE
+third-party/liba/liba.a: FORCE
 	@$(MAKE) --no-print-directory -C third-party/liba
-clean-all::
-	@$(MAKE) --no-print-directory -C third-party/liba clean-all
+clean::
+	@$(MAKE) --no-print-directory -C third-party/liba clean
 
-LIBS-$(CONFIG_USE_LIBB) += third-party/libb/build/libb.so
+LIBS-$(CONFIG_USE_LIBB) += third-party/libb/libb.so
 # libb.so 的制作需要进入 libb 目录去 make
-third-party/libb/build/libb.so: FORCE
+third-party/libb/libb.so: FORCE
 	@$(MAKE) --no-print-directory -C third-party/libb
-clean-all::
-	@$(MAKE) --no-print-directory -C third-party/libb clean-all
+clean::
+	@$(MAKE) --no-print-directory -C third-party/libb clean
